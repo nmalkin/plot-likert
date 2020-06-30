Fix x-axis padding and label calculation (#3)
Previously, the code incorrectly set the interval between tick marks on
the x axis as xaxis.get_tick_space(). (That number actually refers to the
estimated number of ticks that can fit on the axis:
https://matplotlib.org/api/_as_gen/matplotlib.axis.Axis.get_tick_space.html)

That value is always around 10 (based on the standard width of the graph),
so it actually worked pretty well when plotting percentages.
Of course it didn't work so well in other circumstances (#3),
i.e., when you were plotting the total number of responses,
and that number was much smaller (or larger) than 100.

This commit fixes this issue by using xaxis.get_tick_space() value as
intended -- as the total number of tick marks -- and then correctly
calculating the interval that should be between tick marks based on the
maximum value in the underlying data.

Fixing this also helped surface another long-standing issue:
the left pad (no, not *that* left pad) was hard-coded in as an integer
value (5), which (again) worked well for plotting percentages,
but didn't make sense when the data was on another scale.

This commit fixes this by expressing the padding as a fractional value.

plot_likert/plot_likert.py
@@ -31,7 +31,7 @@ except RuntimeError as err:
 from plot_likert.scales import Scale
 import plot_likert.colors as colors
 
-padding_left = 5
+PADDING_LEFT = 0.05  # fraction of the total width to use as padding
 
 
 def plot_counts(
@@ -52,6 +52,7 @@ def plot_counts(
             + counts.iloc[:, scale_middle] / 2
         )
 
+    padding_left = PADDING_LEFT * counts.sum(axis="columns").max()
     center = middles.max() + padding_left
 
     padding_values = (middles - center).abs()
@@ -71,9 +72,10 @@ def plot_counts(
     center_line.set_zorder(-1)
 
     # Compute and show x labels
+    num_ticks = ax.xaxis.get_tick_space()
     max_width = int(round(padded_counts.sum(axis=1).max()))
+    interval = round(max_width / num_ticks)
     right_edge = max_width - center
-    interval = ax.xaxis.get_tick_space()
     right_labels = np.arange(0, right_edge, interval)
     right_values = center + right_labels
     left_labels = np.arange(0, center + 1, interval)
d5884ce4969d117d4384784ff3cdd788e346f220
Nathan Malkin <nmalkin@cs.berkeley.edu>
2020-06-30 12:32:35 -0700

    Add bonus right tick mark
    The current way tick marks on the x axis are generated guarantees that
    there won't be a tick mark higher than the max value.
    In practice, however, this seems to often leave an awkward space at the
    right end of the axis.
    
    This commit adds an extra tick mark;
    because of the padding, I think it should always fit.

--- plot_likert/plot_likert.py
+++ plot_likert/plot_likert.py
@@ -76,7 +76,7 @@ def plot_counts(
     max_width = int(round(padded_counts.sum(axis=1).max()))
     interval = round(max_width / num_ticks)
     right_edge = max_width - center
-    right_labels = np.arange(0, right_edge, interval)
+    right_labels = np.arange(0, right_edge + interval, interval)
     right_values = center + right_labels
     left_labels = np.arange(0, center + 1, interval)
     left_values = center - left_labels

212c9d97f9e3772be6a04d763cf79ceb66118b5a
Nathan Malkin <nmalkin@cs.berkeley.edu>
2021-02-19 21:14:12 -0800

    Release 0.3.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="plot-likert",
-    version="0.3.4",
+    version="0.3.5",
     author="nmalkin",
     description="Library to visualize results from Likert-style survey questions",
     long_description=long_description,

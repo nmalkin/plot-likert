nmalkin/plot-likert

commit be59a472d6fa42047d1dd1fba6e9ee13bc0260a4

Add color scheme with neutral removed

plot_likert/colors.py
@@ -17,6 +17,10 @@ default: Colors = [
     "darkblue",
 ]
 
+# default color scheme with neutral removed
+likert4: Colors = list(plot_likert.colors.default)
+likert4.pop(len(colors4) // 2)
+
 likert5: Colors = ["white", "#d8a539", "#efe0c1", "lightgray", "#bde1dd", "#5ab4ac"]
 
 likert6: Colors = [

commit d9f65e9095f14a04f2767b3ba98d9d957d692a23

Fix variable references for new color
Commit be59a47 introduced a new color scheme but referenced it using an
incorrect name.

plot_likert/colors.py
@@ -18,8 +18,8 @@ default: Colors = [
 ]
 
 # default color scheme with neutral removed
-likert4: Colors = list(plot_likert.colors.default)
-likert4.pop(len(colors4) // 2)
+likert4: Colors = list(default)
+likert4.pop(len(likert4) // 2)
 
 likert5: Colors = ["white", "#d8a539", "#efe0c1", "lightgray", "#bde1dd", "#5ab4ac"]
 
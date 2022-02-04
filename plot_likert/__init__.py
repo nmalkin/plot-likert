055862fad50db60e54c27bbebd6aba901a2399aa
Nathan Malkin <nmalkin@cs.berkeley.edu>
2022-02-03 23:59:14 -0800

    Limit what gets included in import

--- plot_likert/__init__.py
+++ plot_likert/__init__.py
@@ -1,3 +1,11 @@
-from .plot_likert import *
+from .plot_likert import (
+    plot_likert,
+    plot_counts,
+    likert_counts,
+    likert_percentages,
+    likert_response,
+    raw_scale,
+    PlotLikertError,
+)
 
 name = "plot_likert"
